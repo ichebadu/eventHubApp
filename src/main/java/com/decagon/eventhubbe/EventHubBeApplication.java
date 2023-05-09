f5b05d9be1be41052c7c55bf10788b764ac9eafd
francis oyiogu <fcoyiogu@gmail.com>
2023-05-09 22:41:35 +0200

    first

---
+++ src/main/java/com/decagon/eventhubbe/EventHubBeApplication.java
@@ -0,0 +1,13 @@
+package com.decagon.eventhubbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHubBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventHubBeApplication.class, args);
+    }
+
+}
