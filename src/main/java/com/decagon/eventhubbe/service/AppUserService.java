7e06733b426fbcbd64994f60bbe06de45c375379
cyprife0 <karibiwhite5@gmail.com>
2023-05-18 17:16:24 +0100

    Initial Commit

--- src/main/java/com/decagon/eventhubbe/service/AppUserService.java
+++ src/main/java/com/decagon/eventhubbe/service/AppUserService.java
@@ -4,9 +4,13 @@ import com.decagon.eventhubbe.dto.request.LoginRequest;
 import com.decagon.eventhubbe.dto.request.RegistrationRequest;
 import com.decagon.eventhubbe.dto.response.LoginResponse;
 import com.decagon.eventhubbe.dto.response.RegistrationResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface AppUserService {
-    RegistrationResponse register(RegistrationRequest registrationRequest);
+    RegistrationResponse register(RegistrationRequest registrationRequest,
+                                  HttpServletRequest request);
 
     LoginResponse authenticate(LoginRequest loginRequest);
 }
